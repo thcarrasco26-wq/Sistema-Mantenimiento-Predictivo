da06dbe2b889ae571d01e3d4bb6d6f13750cc90a
thcarrasco26-wq <th.carrasco26@gmail.com>
2026-06-01 23:57:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function ingresar(){
 
+let usuario=document.getElementById("user").value;
+let password=document.getElementById("pass").value;
+
+if(usuario!=="" && password!==""){
+
+document.getElementById("loginPage").style.display="none";
+
+document.getElementById("dashboard").style.display="block";
+
+}else{
+
+alert("Ingrese usuario y contraseña");
+
+}
+
+}
+
+function salir(){
+
+document.getElementById("dashboard").style.display="none";
+
+document.getElementById("loginPage").style.display="flex";
+
+}
